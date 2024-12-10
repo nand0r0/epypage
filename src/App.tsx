bf52965f27da2c6da8fd82f602e84c773329503c
nand0r0 <nand0r0gameplay@gmail.com>
2024-12-10 16:07:39 +0100

    aha

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import "./App.scss"
+import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home.tsx"
-import Towergen from "./pages/towergen.tsx"
+import Home from "./pages/home.tsx";
+import Towergen from "./pages/towergen.tsx";
 
 function App() {
-  return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/loadoutgen" element={<Towergen />} />
-      </Routes>
-    </main>
-  );
+	return (
+		<main className="main-content">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/loadoutgen" element={<Towergen />} />
+			</Routes>
+		</main>
+	);
 }
 
 export default App;
