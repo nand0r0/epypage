b029266ed429d58a2c697a0e6b9186a9929af269
nand0r0 <nand0r0gameplay@gmail.com>
2024-12-10 16:17:24 +0100

    maybe it'll work this time

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,26 +1,35 @@
 function Navbar() {
-    return(
-    <>
-        <nav style={{
-            position: "sticky", 
-            top: 0, 
-            padding: "0px 20px", 
-            margin: "0px", 
-            borderRadius: "6px", 
-            backgroundColor: "rgb(14, 17, 24, 0.8)"
-        }}>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/loadoutgen">Loadout generator</a></li>
-            </ul>
-            <ul>
-                <li><a href="https://github.com/nand0r0" target="blank">GitHub</a></li>
-            </ul>
-            
-        </nav>
-        <hr />
-    </>
-    )
+	return (
+		<>
+			<nav
+				style={{
+					position: "sticky",
+					top: 0,
+					padding: "0px 20px",
+					margin: "0px",
+					borderRadius: "6px",
+					backgroundColor: "rgb(14, 17, 24, 0.8)",
+				}}
+			>
+				<ul>
+					<li>
+						<a href="/epypage">Home</a>
+					</li>
+					<li>
+						<a href="/epypage/loadoutgen">Loadout generator</a>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<a href="https://github.com/nand0r0" target="blank">
+							GitHub
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<hr />
+		</>
+	);
 }
 
-export default Navbar
+export default Navbar;
